59969b5da600bf847fcc8b3841a415b012774533
Mehdi <mehdi.bchir@esprit.tn>
2024-02-19 16:40:07 +0100

    update

---
+++ src/Controller/User2Controller.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+#[Route('/user2')]
+class User2Controller extends AbstractController
+{
+    #[Route('/', name: 'app_user2_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
+    {
+        return $this->render('user2/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_user2_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user2_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user2/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user2_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user2/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_user2_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user2_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user2/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user2_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_user2_index', [], Response::HTTP_SEE_OTHER);
+    }
+    #[Route('/{id}', name: 'app_user2_sort', methods: ['POST'])]
+    private function sortUsers(string $sort, EntityManagerInterface $entityManager): array
+    {
+        // Vérifier si le paramètre de tri est valide
+        $validSortFields = ['id', 'email', 'name', 'prename', 'phone', 'isVerified', 'birthday', 'image'];
+        if (!in_array($sort, $validSortFields)) {
+            throw new \InvalidArgumentException('Invalid sort field');
+        }
+
+        // Récupérer les utilisateurs triés selon le paramètre de tri
+        return $entityManager->getRepository(User::class)->findBy([], [$sort => 'ASC']);
+    }
+    #[Route('/listePdf/download', name:'listePdf')]
+     
+    public function listePdf(EntityManagerInterface $entityManager): Response
+    {
+        $user = $entityManager
+        ->getRepository(User::class)
+        ->findAll();
+
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('user2/listePdf.html.twig', [
+            'users' => $user,
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        return new Response($dompdf->output(), 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'attachment; filename="mypdf.pdf"',
+        ]);
+
+
+
+    }
+}
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Regex;
+
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        /*->add('ProfilePicture', FileType::class, [
+            'mapped' => false,
+            'required' => false,
+            'label' => 'Profile picture :',
+            'attr' => [
+                'class' => 'form-control',
+                'id' => 'formFile',
+            ],
+            'constraints' => [
+                new File([
+                    'maxSize' => '1024k',
+                    'mimeTypes' => [
+                        'image/*',"image/jpeg" , "image/png" , "image/tiff" , "image/svg+xml", "image/gif", "image/webp",
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid image file',
+                ])
+            ],
+        ])*/
+            ->add('name', null, [
+                'label' => 'Name',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Your name',
+                    'id' => 'nameBasic'
+                ],
+                'constraints' => [new NotBlank()]
+                ])
+            ->add('prename', null, [
+                'label' => 'Prename',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Your prename',
+                    'id' => 'nameBasic',
+                ],
+                'constraints' => [new NotBlank()]
+                ])
+            ->add('email', null, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your email',
+                    'id' => 'email',
+                ],
+                'constraints' => [new NotBlank()]
+                ])
+                ->add('phone', TextType::class, [
+                    'label' => 'Phone :',
+                    'required' => false,
+                    'attr' => [
+                        'class' => 'form-control',
+                        'id' => 'phone',
+                        'maxlength' => 8, // Limiter à 8 caractères
+                        'minlength' => 8, // Limiter à 8 caractères
+
+                        'pattern' => '^[0-9]*$', // Uniquement des chiffres
+                    ],
+                    'constraints' => [
+                        new Regex([
+                            'pattern' => '/^[0-9]*$/',
+                            'message' => 'Le numéro de téléphone ne peut contenir que des chiffres.',
+                        ]),
+                    ],
+                ])
+            ->add('birthday', DateType::class, [
+                'label' => 'Birthday',
+                'html5' => true,
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control',
+                    'id' => 'html5-date-input'
+                ],
+                'constraints' => [new NotBlank()]
+                ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Password',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Enter your password',
+                    'id' => 'password'
+                ],
+                'constraints' => [new NotBlank()]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
